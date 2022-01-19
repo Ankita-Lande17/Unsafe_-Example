9c9930b3d3ecaf2633fe9d47d6a785d598fb394e
“Ankita <ankita.lande@netsolutions.com>
2022-01-19 16:33:10 +0530

    unsafe program updated.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Unsafe_Example{
+class UnsafeTest
+{
+    unsafe static void SquarePtrParam(int* p)
+    {
+        *p *= *p;
+    }
+
+    unsafe static void Main()
+    {
+        int i = 12;
+    
+        SquarePtrParam(&i);
+        Console.WriteLine(i);
+    }
+}
+
+}
